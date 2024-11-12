21234599311626ae1bc20d213aef3d65b70fe855
Alexandru Maftei <mafterr11@gmail.com>
2024-11-12 14:15:14 +0200

    .

--- components/home/Servicii.jsx
+++ components/home/Servicii.jsx
@@ -1,37 +1,39 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 const servicii = [
   {
-    name: "Extinderi retea gaze naturale",
+    name: "Extinderi rețea gaze naturale",
   },
   {
-    name: "Bransamente gaze naturale",
+    name: "Branșamente gaze naturale",
   },
   {
-    name: "Instalatii gaze naturale",
+    name: "Instalații gaze naturale",
   },
   {
-    name: "Instalatii sanitare/termice",
+    name: "Instalații sanitare/termice",
   },
   {
-    name: "Verificari instalatii de utilizare",
+    name: "Verificări instalații de utilizare",
   },
   {
-    name: "Revizii instalatii de utilizare",
+    name: "Revizii instalații de utilizare",
   },
   {
-    name: "Verificari/revizii/puneri in functiune centrale termice",
+    name: "Verificări/revizii/puneri în funcțiune centrale termice",
   },
   {
-    name: "Consultanta tehnica",
+    name: "Consultanță tehnică",
   },
 ];
 
+
 export default function Servicii() {
   return (
-    <div className="container mx-auto max-w-screen-lg px-4 py-24 md:px-8">
-      <div className="flex w-full flex-col items-center justify-center gap-x-12 gap-y-8 md:flex-row">
+    <div className="container mx-auto max-w-screen-xl px-4 py-24 md:px-8">
+      <div className="flex w-full flex-col items-center justify-center gap-x-24 gap-y-8 md:flex-row">
         {/* Imagine container */}
         <div className="flex h-60 w-full justify-center md:h-[55vh] md:justify-start">
           <div className="relative h-full w-[80%] border-2 border-orange md:w-full">
@@ -45,24 +47,29 @@ export default function Servicii() {
           </div>
         </div>
         {/* Text container */}
-        <div>
-          <h3>Bava Gaz Construct SRL</h3>
-          <h2>
-            Instalații De Gaze Și Servicii Sanitare - Oricând La Dispoziția
-            Dumneavoastră
-          </h2>
-          <p>
+        <div className="flex flex-col gap-y-3">
+          <div>
+            <h3 className="text-orange">Bava Gaz Construct SRL</h3>
+            <h2>
+              Instalații De Gaze Și Servicii Sanitare - Oricând La Dispoziția
+              Dumneavoastră
+            </h2>
+          </div>
+          <p className="mb-2">
             Specializați în instalații de gaze și sisteme sanitare, oferim
             servicii de încredere pentru siguranța și confortul casei dvs.
           </p>
-          {servicii.map((serviciu, index) => {
-            return (
-              <ul key={index}>
-                <li>{serviciu.name}</li>
-              </ul>
-            );
-          })}
-          <Button>Citeste mai mult</Button>
+          <ul className="mb-5 list-disc pl-5 marker:text-orange">
+            {servicii.map((serviciu, index) => {
+              return <li key={index}>{serviciu.name}</li>;
+            })}
+          </ul>
+
+          <div>
+            <Link href={"/servicii"}>
+            <Button>Citește mai mult</Button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
