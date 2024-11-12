3d8ac69de6f813692b2afa68dca69bb3fee44ddc
Alexandru Maftei <mafterr11@gmail.com>
2024-11-12 13:32:04 +0200

    .

--- components/home/Servicii.jsx
+++ components/home/Servicii.jsx
@@ -1,12 +1,39 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
 
+const servicii = [
+  {
+    name: "Extinderi retea gaze naturale",
+  },
+  {
+    name: "Bransamente gaze naturale",
+  },
+  {
+    name: "Instalatii gaze naturale",
+  },
+  {
+    name: "Instalatii sanitare/termice",
+  },
+  {
+    name: "Verificari instalatii de utilizare",
+  },
+  {
+    name: "Revizii instalatii de utilizare",
+  },
+  {
+    name: "Verificari/revizii/puneri in functiune centrale termice",
+  },
+  {
+    name: "Consultanta tehnica",
+  },
+];
+
 export default function Servicii() {
   return (
     <div className="container mx-auto max-w-screen-lg px-4 py-24 md:px-8">
       <div className="flex w-full flex-col items-center justify-center gap-x-12 gap-y-8 md:flex-row">
         {/* Imagine container */}
-        <div className="flex h-60 w-full justify-center md:h-[55vh]  md:justify-start">
+        <div className="flex h-60 w-full justify-center md:h-[55vh] md:justify-start">
           <div className="relative h-full w-[80%] border-2 border-orange md:w-full">
             <Image
               alt="De ce sa alegeti Bava Gas Grup"
@@ -28,6 +55,13 @@ export default function Servicii() {
             Specializați în instalații de gaze și sisteme sanitare, oferim
             servicii de încredere pentru siguranța și confortul casei dvs.
           </p>
+          {servicii.map((serviciu, index) => {
+            return (
+              <ul key={index}>
+                <li>{serviciu.name}</li>
+              </ul>
+            );
+          })}
           <Button>Citeste mai mult</Button>
         </div>
       </div>
