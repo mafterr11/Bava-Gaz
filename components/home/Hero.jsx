e59ca4711cad9e33d2cb1d498607ceefcc8d8b9e
Alexandru Maftei <mafterr11@gmail.com>
2024-11-13 13:42:51 +0200

    .

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -7,22 +7,18 @@ export default function Hero() {
       {/* Black overlay */}
       <div className="absolute inset-0 bg-[#0D0C0BA6]" />
       {/* Hero container */}
-      <div className="flex pt-40 xl:h-full xl:pt-0 xl:pb-20 flex-col items-center justify-center xl:px-20 2xl:px-0">
+      <div className="flex flex-col items-center justify-center pt-40 xl:h-full xl:px-20 xl:pb-20 xl:pt-0 2xl:px-0">
         <div className="relative z-30 text-center font-nunitoSans">
           <h3 className="mb-3 font-semibold text-orange">Bava Gaz Construct</h3>
           <h1 className="mb-6 px-2 md:max-xl:px-7 xl:max-w-3xl">
             Instalații Gaz, Electrice, Sanitare, Automatizări, Consultanță,
             Proiectare
           </h1>
-            <Button>
-            <a
-            href="tel:+40755313662"
-            target="blank"
-            className="text-lg"
-          >
-            +40.755.313.662
-          </a>
-            </Button>
+          <Button>
+            <a href="tel:+40755313662" target="blank" className="text-lg">
+              +40.755.313.662
+            </a>
+          </Button>
         </div>
       </div>
     </div>
