2b323cb3bfa26003a3f49c3ac8ed6bd243bfba20
Alexandru Maftei <mafterr11@gmail.com>
2024-11-13 14:39:39 +0200

    .

--- components/home/CTA.jsx
+++ components/home/CTA.jsx
@@ -6,13 +6,13 @@ const Cta = () => {
   return (
     <section className="bg-tools relative mb-44 bg-cover bg-fixed py-28">
       <div className="absolute inset-0 bg-blue/85" />
-      <div className="container relative mx-auto md:max-xl:max-w-[90%]">
+      <div className="md:container relative mx-auto md:max-xl:max-w-[90%]">
         <div className="flex flex-col items-center">
           <div className="mb-6 flex flex-col items-center justify-center gap-y-2 text-center xl:max-w-5xl">
             <h3 className="text-orange">
               Ajutor profesionist pentru instalația de gaze?
             </h3>
-            <h2>
+            <h2 className="text-balance">
               Specialiștii noștri sunt la dispoziția dumneavoastră.
               Contactați-ne astăzi pentru o consultație gratuită!
             </h2>
