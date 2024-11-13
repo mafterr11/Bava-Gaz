8bf0aac48f57041cc0e023f605e095b210362d1f
Alexandru Maftei <mafterr11@gmail.com>
2024-11-13 15:36:13 +0200

    .

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
       <div className="flex flex-col items-center justify-center pt-40 xl:h-full xl:px-20 xl:pb-20 xl:pt-0 2xl:px-0">
         <div className="relative z-30 text-center font-nunitoSans">
           <h3 className="mb-3 font-semibold text-orange">Bava Gaz Construct</h3>
-          <h1 className="mb-6 px-2 md:max-xl:px-7 xl:max-w-3xl">
+          <h1 className="mb-6 px-2 md:max-xl:px-7 xl:max-w-5xl">
             Instalații Gaz, Electrice, Sanitare, Automatizări, Consultanță,
             Proiectare
           </h1>
